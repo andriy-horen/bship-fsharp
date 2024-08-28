eaee5a91879a55e6e82003359d656a7bf9106fbc
Andriy Horen <go.andriy@gmail.com>
2024-08-29 00:35:10 +0200

    Implement client disconnect in the hub

--- src/Bship/GameHub.fs
+++ src/Bship/GameHub.fs
@@ -10,19 +10,19 @@ type GameHub() as self =
     static let games =
         ConcurrentDictionary<string, Tuple<HubCallerContext, HubCallerContext>>()
 
-    static let waiting = ConcurrentDictionary<string, HubCallerContext>()
+    static let waitingClients = ConcurrentDictionary<string, HubCallerContext>()
 
     override this.OnConnectedAsync() =
         task {
-            match waiting.Count with
+            match waitingClients.Count with
             // no one to pair with, adding to a waiting list
-            | 0 -> waiting.TryAdd(this.Context.ConnectionId, this.Context) |> ignore
+            | 0 -> waitingClients.TryAdd(this.Context.ConnectionId, this.Context) |> ignore
             // pair with other client
             | _ ->
                 let groupId = Guid.NewGuid()
-                
-                let otherClient = waiting |> Seq.head
-                waiting.TryRemove(otherClient) |> ignore
+
+                let otherClient = waitingClients |> Seq.head
+                waitingClients.TryRemove(otherClient) |> ignore
 
                 this.Context.Items.Add("gameId", groupId.ToString())
                 games.TryAdd(groupId.ToString(), (this.Context, otherClient.Value)) |> ignore
@@ -35,6 +35,13 @@ type GameHub() as self =
 
     override this.OnDisconnectedAsync(ex) =
         let connectionId = this.Context.ConnectionId
-        waiting.TryRemove connectionId |> ignore
+        waitingClients.TryRemove connectionId |> ignore
+
+        // TODO: client might not have been paired, so reading gameId will probably throw
+        let gameId = this.Context.Items["gameId"] :?> string
+        let success, (client1, client2) = games.TryGetValue gameId
+        if success then
+            client1.Abort()
+            client2.Abort()
 
         self.OnDisconnectedAsync(ex)
