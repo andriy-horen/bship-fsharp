c8379c476094c5d7e60f9e82ebf4efbf388a86d6
Andriy Horen <go.andriy@gmail.com>
2024-08-29 16:10:17 +0200

    Implement hub & logging

--- src/Bship/GameHub.fs
+++ src/Bship/GameHub.fs
@@ -3,8 +3,22 @@ module Bship.GameHub
 open System
 open System.Collections.Concurrent
 open Microsoft.AspNetCore.SignalR
+open Microsoft.Extensions.Logging
 
-type GameHub() as self =
+let groupIdKey = "groupId"
+
+let addWaiting
+    (logger: ILogger)
+    (waitingClients: ConcurrentDictionary<string, HubCallerContext>)
+    (client: HubCallerContext)
+    =
+    let connectionId = client.ConnectionId
+
+    let added = waitingClients.TryAdd(connectionId, client)
+    logger.LogInformation("Adding client {ConnectionId} to the waiting list {Success}", connectionId, added)
+    added
+
+type GameHub(logger: ILogger<GameHub>) as self =
     inherit Hub()
 
     static let games =
@@ -13,22 +27,41 @@ type GameHub() as self =
     static let waitingClients = ConcurrentDictionary<string, HubCallerContext>()
 
     override this.OnConnectedAsync() =
+        logger.LogWarning this.Context.ConnectionId
+
         task {
             match waitingClients.Count with
             // no one to pair with, adding to a waiting list
-            | 0 -> waitingClients.TryAdd(this.Context.ConnectionId, this.Context) |> ignore
+            | 0 -> addWaiting logger waitingClients this.Context |> ignore
             // pair with other client
             | _ ->
                 let groupId = Guid.NewGuid()
 
                 let otherClient = waitingClients |> Seq.head
-                waitingClients.TryRemove(otherClient) |> ignore
+                let removed = waitingClients.TryRemove(otherClient)
 
-                this.Context.Items.Add("gameId", groupId.ToString())
-                games.TryAdd(groupId.ToString(), (this.Context, otherClient.Value)) |> ignore
+                logger.LogInformation(
+                    "Retrieving a client {ConnectionId} from the waiting list {Success}",
+                    otherClient.Value.ConnectionId,
+                    removed
+                )
+
+                if removed = false then
+                    addWaiting logger waitingClients this.Context |> ignore
+
+                this.Context.Items.Add(groupIdKey, groupId.ToString())
+                let added = games.TryAdd(groupId.ToString(), (this.Context, otherClient.Value))
 
                 do! this.Groups.AddToGroupAsync(this.Context.ConnectionId, groupId.ToString())
                 do! this.Groups.AddToGroupAsync(otherClient.Value.ConnectionId, groupId.ToString())
+
+                logger.LogInformation(
+                    "Created a group {GroupId} with two clients {Connection1} {Connection2} {Success}",
+                    groupId,
+                    this.Context.ConnectionId,
+                    otherClient.Value.ConnectionId,
+                    added
+                )
 
             do! self.OnConnectedAsync()
         }
@@ -39,7 +72,7 @@ type GameHub() as self =
         if waitingClients.ContainsKey connectionId then
             waitingClients.TryRemove connectionId |> ignore
 
-        let _, gameId = this.Context.Items.TryGetValue "gameId"
+        let _, gameId = this.Context.Items.TryGetValue groupIdKey
         // if connection has a gameId then disconnect both clients
         if gameId :? string then
             let gameId = gameId :?> string
--- src/Bship/Program.fs
+++ src/Bship/Program.fs
@@ -5,22 +5,39 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
 open Giraffe
 
 let webApp = choose [ route "/" >=> text "Hello F# Api" ]
 
 let configureApp (app: IApplicationBuilder) =
     app
+        .UseCors(fun cors ->
+            cors
+                .WithOrigins("http://localhost:8081")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+            |> ignore)
+        .UseRouting()
         .UseEndpoints(fun endpoints -> endpoints.MapHub<GameHub>("/game") |> ignore)
         .UseGiraffe
         webApp
 
 let configureServices (services: IServiceCollection) =
+    services
+        .AddCors()
+        .AddLogging()
+        .AddGiraffe()
+    |> ignore
+
     services.AddSignalR() |> ignore
-    services.AddGiraffe() |> ignore
 
 let configure (webHostBuilder: IWebHostBuilder) =
-    webHostBuilder.Configure(configureApp).ConfigureServices(configureServices)
+    webHostBuilder
+        .ConfigureLogging(fun logging -> logging.ClearProviders().AddConsole() |> ignore)
+        .Configure(configureApp)
+        .ConfigureServices(configureServices)
 
 [<EntryPoint>]
 let main _ =
