544a4b03cd11f41687982bac9ce190cc168eea0a
Andriy Horen <go.andriy@gmail.com>
2025-02-09 01:43:01 +0100

    chore: add react boilerplate

---
+++ client/src/app/router.tsx
@@ -0,0 +1,30 @@
+import { QueryClient, useQueryClient } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+
+const convert = (queryClient: QueryClient) => (m: any) => {
+  const { clientLoader, clientAction, default: Component, ...rest } = m;
+  return {
+    ...rest,
+    loader: clientLoader?.(queryClient),
+    action: clientAction?.(queryClient),
+    Component,
+  };
+};
+
+export const createAppRouter = (queryClient: QueryClient) =>
+  createBrowserRouter([
+    {
+      path: '*',
+      lazy: () => import('./routes/not-found').then(convert(queryClient)),
+    },
+  ]);
+
+export const AppRouter = () => {
+  const queryClient = useQueryClient();
+
+  const router = useMemo(() => createAppRouter(queryClient), [queryClient]);
+
+  return <RouterProvider router={router} />;
+};
