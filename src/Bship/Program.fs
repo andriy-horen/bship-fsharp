85a9dcc7f5a627df65a702f69b9f21b8e1409710
Andriy Horen <go.andriy@gmail.com>
2025-02-13 23:46:33 +0100

    chore: update cors port

--- src/Bship/Program.fs
+++ src/Bship/Program.fs
@@ -108,7 +108,7 @@ let main args =
     app
         .UseCors(fun cors ->
             cors
-                .WithOrigins([| 4200 |] |> Array.map (fun port -> $"http://localhost:{port}"))
+                .WithOrigins([| 5173 |] |> Array.map (fun port -> $"http://localhost:{port}"))
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials()
